6c1268a38e45424791a986559444c47049a365d8
SoDDoS <shaxrussian@gmail.com>
2025-05-01 20:59:50 +0300

    Redesign logistics page with tabs for arrived/departed items and product details

--- apps/logistics/urls.py
+++ apps/logistics/urls.py
@@ -7,4 +7,6 @@ app_name = 'logistics'
 urlpatterns = [
     path('', views.transport_list, name='transport_list'),
     path('add/', views.transport_add, name='transport_add'),
+    path('transport/<int:pk>/detail/', views.transport_detail, name='transport_detail'),
+    path('product/<int:pk>/card/', views.product_card_view, name='product_card'),
 ]
--- apps/logistics/views.py
+++ apps/logistics/views.py
@@ -1,18 +1,32 @@
 # apps/logistics/views.py
-from django.shortcuts import render, redirect
-from django.http import Http404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import Http404, HttpResponse
 from .models import Transport, TransportProduct
 from apps.warehouse.models import Stock, Warehouse
 from apps.catalog.models import Product
 from .forms import TransportForm, TransportProductForm
 from apps.catalog.models import Category
 from django.db import transaction
+from django.template.loader import render_to_string
 
 def transport_list(request):
     transports = Transport.objects.all().order_by('-date')
     return render(request, 'logistics/transport_list.html', {'transports': transports})
 
+def transport_detail(request, pk):
+    transport = get_object_or_404(Transport, pk=pk)
+    products = transport.transportproduct_set.select_related('product')
+    html = render_to_string('logistics/transport_detail_panel.html', {
+        'transport': transport,
+        'products': products
+    })
+    return HttpResponse(html)
 
+
+
+def product_card_view(request, pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, 'logistics/transport_product_card.html', {'product': product})
 
 
 def transport_add(request):
